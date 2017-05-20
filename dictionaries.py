7168b77813497273baa5b535c8dcf255b26c1cbb
Sagar Chamling <sgr.raee@gmail.com>
2017-05-20 21:57:02 +0545

    Dictionary Practice

---
+++ dictionaries.py
@@ -0,0 +1,11 @@
+# Working on dictionaries
+
+dictionaries_content = {
+    'Sagar': 23,
+    'Sudesh': 18,
+    'Sujal': 13
+}
+
+name = 'Sujal'
+
+print(name, 'age is', dictionaries_content[name])
