ce812921dd83c3d939bfe69ca1c37d302cc54e27
Sagar Chamling <sgr.raee@gmail.com>
2017-05-20 17:48:45 +0545

    Function + Tuple

--- function.py
+++ function.py
@@ -1,5 +1,5 @@
 """
-# Working on Function
+# Working on Function & Tuple Return
 """
 
 
@@ -8,5 +8,14 @@ def add(num1, num2=5):
 
 
 print(add(5, 2))
-
 print(add(5))
+
+
+def get_tuple_value():
+    return 'apple', 'ball'
+
+
+x, y = get_tuple_value()
+
+print('x: ', x)
+print('y: ', y)
