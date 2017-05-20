1c1c2a69b31fe2ca5f6a601f7bdfc3b076e671f9
Sagar Chamling <sgr.raee@gmail.com>
2017-05-20 23:22:18 +0545

    Tkinter with exit button

--- tkinter-demo.py
+++ tkinter-demo.py
@@ -2,14 +2,24 @@ from tkinter import *
 
 
 class Window(Frame):
-    """docstring for Window"""
-
     def __init__(self, master=None):
         Frame.__init__(self, master)
-
         self.master = master
+        self.init_window()
+
+    def init_window(self):
+        self.master.title("Tic Tac Toe")
+        self.pack(fill=BOTH, expand=1)
+        quit_button = Button(self, text="Quit", command=self.client_exit)
+        quit_button.place(x=0, y=0)
+
+    @staticmethod
+    def client_exit():
+        exit()
 
 
 root = Tk()
+root.geometry("400x300")
+
 app = Window(root)
 root.mainloop()
